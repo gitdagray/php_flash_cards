2ab6b5ef5e8974e8f3c8623abaff5c19beddaffd
gitdagray <DaveOnEleven@gmail.com>
2021-01-30 13:04:31 -0600

    styling updates and a11y

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Times Tables</title>
+    <title>Multiplication Flash Cards</title>
     <link rel="stylesheet" href="css/main.min.css">
 </head>
 <body>
--- view/results.php
+++ view/results.php
@@ -1,13 +1,15 @@
-<main class="main-results">
+<main class="main-results" aria-label="flash card container">
     <?php
         $cards = null;
         for ($i = 1; $i <= 12; $i++) {
             $result = $number * $i;
-            $cards .= "<div class='card' tabindex='0'>
+            $cards .= "<div class='card' tabindex='0'
+                        aria-label='{$number} multiplied by {$i} equals {$result}'>
                         <div class='card-front'>
                         {$number} x {$i}
                     </div>
-                    <div class='card-back'>
+                    <div class='card-back' 
+                        aria-label='{$number} multiplied by {$i} equals {$result}'>
                         {$result}
                     </div>
                 </div>";
